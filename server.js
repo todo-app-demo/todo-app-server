e90f0b850307a4cf4e6e85b761a919438f2cef2d
Allie <allie@codefellows.com>
2017-11-08 18:17:15 -0800

    adding admin demo

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const pg = require('pg');
 const bodyParser = require('body-parser').urlencoded({extended: true});
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
+const TOKEN = process.env.TOKEN;
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
@@ -23,13 +24,17 @@ app.get('/tasks', (req, res) => {
     .catch(console.error);
 });
 
+app.get('/admin', (req, res) => res.send(TOKEN === parseInt(req.query.token)))
+// app.get('/admin', (req, res) => console.log(req.query.token))
+
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
 app.post('/tasks/add', bodyParser, (req, res) => {
   let {title, description, category, contact, status} = req.body;
 
   client.query(`
-      INSERT INTO tasks(title, description, category, contact, status) VALUES($1, $2, $3, $4, $5)`,
+      INSERT INTO tasks(title, description, category, contact, status)
+      VALUES ($1, $2, $3, $4, $5)`,
       [title, description, category, contact, status]
     )
     .then(results => res.sendStatus(201))
